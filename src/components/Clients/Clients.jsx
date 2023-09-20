c11f11f1e4642a7747742a41b09bcc4edb16a32d
Harun Hadzagic <harun.hadzagic@gmail.com>
2023-09-20 16:09:29 +0200

    Update Clients.jsx

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -22,15 +22,15 @@ let clients = [
         link: "https://www.linkedin.com/in/amila-omanovi%C4%87-a9764311a/",
         disc : `Harun is a committed team member and one of key drivers of our high client satisfaction scores. He sets very ambitious goals for himself and what’s impressive is that he manages to meet them all. I appreciate that I can always count on him to treat all his tasks with importance. That’s what makes him a reliable team member. Furthermore, he’s an excellent communicator and keeps me up to date on his progress, ensuring we’re always on the same page. He often shows initiative, has a great eye for detail and consistently produces high work quality. `
     },
-    {
-        name : "Sulejman Catibusic",
-        position : "a programmer at heart",
-        img_url : "https://media.licdn.com/dms/image/C5603AQEUAEK7XunMpg/profile-displayphoto-shrink_800_800/0/1517532784158?e=1700092800&v=beta&t=yUSYXzvpuLlura-IYrPo4RHQ7Vw7G8c-MAIeEIENRV0",
-        stars : 5,
-        link:"https://www.linkedin.com/in/sulejman-catibusic-2991354a/",
-        disc : `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-        Temporibus consequuntur dolores labore natus similique nemo doloribus cum accusantium adipisci maiores.`
-    },
+    // {
+    //     name : "Sulejman Catibusic",
+    //     position : "a programmer at heart",
+    //     img_url : "https://media.licdn.com/dms/image/C5603AQEUAEK7XunMpg/profile-displayphoto-shrink_800_800/0/1517532784158?e=1700092800&v=beta&t=yUSYXzvpuLlura-IYrPo4RHQ7Vw7G8c-MAIeEIENRV0",
+    //     stars : 5,
+    //     link:"https://www.linkedin.com/in/sulejman-catibusic-2991354a/",
+    //     disc : `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+    //     Temporibus consequuntur dolores labore natus similique nemo doloribus cum accusantium adipisci maiores.`
+    // },
     {
         name : "Ajsela Felic",
         position : "UI/UX designer",
